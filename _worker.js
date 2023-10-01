c01683199f52cc081de705155f7e666676e6c324
stander <std@std.lv>
2023-10-02 01:20:52 +0300

    test func with fetch

--- _worker.js
+++ _worker.js
@@ -2,37 +2,44 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     if (url.pathname.startsWith('/api')) {
-      const someHost = "http://ledg.app/mainnet/api/";
+      const api_address = "http://ledg.app/mainnet/api";
 
-      const url = someHost //+ "/static/json";
+      const statusCode = 301;
 
+      const url = new URL(request.url);
+      const { pathname, search } = url;
 
-      /**
-       * gatherResponse awaits and returns a response body as a string.
-       * Use await gatherResponse(..) in an async function to get the response body
-       * @param {Response} response
-       */
-      async function gatherResponse(response) {
-        const { headers } = response;
-        const contentType = headers.get("content-type") || "";
-        if (contentType.includes("application/json")) {
-          return `url: ${url} \n`+JSON.stringify(await response.json());
-        }
-        return response.text();
-      }
+      const destinationURL = `${api_address}${pathname}${search}`;
+      console.log(destinationURL);
 
-
-      const init = {
-        headers: {
-          "content-type": "application/json;charset=UTF-8",
-        },
-      };
-
-      const response = await fetch(url, init);
-
-      //console.log(response)
-      const results = await gatherResponse(response);
-      return new Response(results, init);
+      return Response.redirect(destinationURL, statusCode);
+      // const url = someHost //+ "/static/json";
+      // /**
+      //  * gatherResponse awaits and returns a response body as a string.
+      //  * Use await gatherResponse(..) in an async function to get the response body
+      //  * @param {Response} response
+      //  */
+      // async function gatherResponse(response) {
+      //   const { headers } = response;
+      //   const contentType = headers.get("content-type") || "";
+      //   if (contentType.includes("application/json")) {
+      //     return `url: ${url} \n`+JSON.stringify(await response.json());
+      //   }
+      //   return `url2: ${url} \n`+response.text();
+      // }
+      //
+      //
+      // const init = {
+      //   headers: {
+      //     "content-type": "application/json;charset=UTF-8",
+      //   },
+      // };
+      //
+      // const response = await fetch(url, init);
+      //
+      // //console.log(response)
+      // const results = await gatherResponse(response);
+      // return new Response(results, init);
     }
 
     return env.ASSETS.fetch(request);
