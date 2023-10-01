2a491d34e71d835a4052c0cd84936b8d6541bc49
stander <std@std.lv>
2023-10-01 23:43:52 +0300

    test func with fetch

--- functions/func.js
+++ functions/func.js
@@ -11,26 +11,26 @@ export async function onRequest(context) {
      * Use await gatherResponse(..) in an async function to get the response body
      * @param {Response} response
      */
-    async function gatherResponse(response) {
-      const { headers } = response;
-      const contentType = headers.get("content-type") || "";
-      if (contentType.includes("application/json")) {
-        return JSON.stringify(await response.json());
-      }
-      return response.text();
-    }
-
-
-    const init = {
-      headers: {
-        "content-type": "application/json;charset=UTF-8",
-      },
-    };
-
-    const response = await fetch(url, init);
-    //const results=JSON.stringify(response)
-    const results = await gatherResponse(response);
-    //console.log({res:url})
+    // async function gatherResponse(response) {
+    //   const { headers } = response;
+    //   const contentType = headers.get("content-type") || "";
+    //   if (contentType.includes("application/json")) {
+    //     return JSON.stringify(await response.json());
+    //   }
+    //   return response.text();
+    // }
+    //
+    //
+    // const init = {
+    //   headers: {
+    //     "content-type": "application/json;charset=UTF-8",
+    //   },
+    // };
+    //
+    // const response = await fetch(url, init);
+    //
+    // const results = await gatherResponse(response);
+    //
     
-  return new Response(results)
+  return new Response(url)
 }
