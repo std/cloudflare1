248de932f253295f51fc69edc1fe5c57e257d24a
stander <std@std.lv>
2023-10-02 00:41:18 +0300

    test func with fetch

--- _worker.js
+++ _worker.js
@@ -1,10 +1,37 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    if (url.pathname.startsWith('/api/')) {
-      // TODO: Add your custom /api/* logic here.
-      console.log('api')
-      return new Response('Ok');
+    if (url.pathname.startsWith('/api')) {
+      const someHost = "http://ledg.app/mainnet/api/Sector";
+
+      const url = someHost //+ "/static/json";
+
+      /**
+       * gatherResponse awaits and returns a response body as a string.
+       * Use await gatherResponse(..) in an async function to get the response body
+       * @param {Response} response
+       */
+      async function gatherResponse(response) {
+        const { headers } = response;
+        const contentType = headers.get("content-type") || "";
+        if (contentType.includes("application/json")) {
+          return JSON.stringify(await response.json());
+        }
+        return response.text();
+      }
+
+
+      const init = {
+        headers: {
+          "content-type": "application/json;charset=UTF-8",
+        },
+      };
+
+      const response = await fetch(url, init);
+
+      //console.log(response)
+      const results = await gatherResponse(response);
+      return new Response(results, init);
     }
     // Otherwise, serve the static assets.
     // Without this, the Worker will error and no assets will be served.
