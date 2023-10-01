720a845db8b5587b89983d4debf6b2f9c1e58831
stander <std@std.lv>
2023-10-01 20:05:40 +0300

    test func with fetch

--- functions/func.js
+++ functions/func.js
@@ -1,3 +1,33 @@
+
 export function onRequest(context) {
-  return new Response("this is  page func run!")
+
+    const someHost = "http://ledg.app/mainnet/api/Sector";
+
+    const url = someHost + "/mainnet/api/Account?id=eq.101";
+
+    /**
+     * gatherResponse awaits and returns a response body as a string.
+     * Use await gatherResponse(..) in an async function to get the response body
+     * @param {Response} response
+     */
+    async function gatherResponse(response) {
+      const { headers } = response;
+      const contentType = headers.get("content-type") || "";
+      if (contentType.includes("application/json")) {
+        return JSON.stringify(await response.json());
+      }
+      return response.text();
+    }
+
+
+    const init = {
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+      },
+    };
+
+    const response = await fetch(url, init);
+    const results = await gatherResponse(response);
+    
+  return new Response(results)
 }
