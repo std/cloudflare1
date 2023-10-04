394b596685c325cc5721abbda65665252404ea88
stander <std@std.lv>
2023-10-04 03:25:21 +0300

    try run  pages function with custom domain

--- _worker.js
+++ _worker.js
@@ -1,15 +1,21 @@
 export default {
   async fetch(request, env,ctx) {
     const url = new URL(request.url);
-    if (url.pathname.startsWith('/api')) {
-      const api_address = "http://ledg.app/mainnet";
 
-      //const statusCode = 301;
+
+    const api_address = "http://ledg.app/mainnet";
+
+    // const { pathname, search } = url;
+    // const destinationURL = `${api_address}${pathname}${search}`;
+
+    if (url.pathname.startsWith('/api')) {
 
       const url = new URL(request.url);
       const { pathname, search } = url;
 
       const destinationURL = `${api_address}${pathname}${search}`;
+      //return new Response(JSON.stringify({destinationURL, data:await fetch(destinationURL)}))
+      //return new Response(`api: ${destinationURL}`)
 
       async function gatherResponse(response) {
         const { headers } = response;
@@ -26,10 +32,15 @@ export default {
         },
       };
 
+      //const response = await fetch(destinationURL, init);
+      //
+      // const results = await gatherResponse(response);
+      // const response = new Response(results, init);
+
       const cache=caches.default
       const cacheKey=destinationURL
       let  response=await cache.match(cacheKey);
-
+      //let response=undefined
       if (!response) {
         response = await fetch(destinationURL, init);
 
@@ -40,7 +51,7 @@ export default {
       }
       return response
     }
-
+    //return new Response(url)
     return env.ASSETS.fetch(request);
   },
 }
