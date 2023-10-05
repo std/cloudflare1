32d328663232dee8b8f0ba5d051e1052ccf2fa12
stander <std@std.lv>
2023-10-05 16:54:36 +0300

    fix headers

--- _worker.js
+++ _worker.js
@@ -45,7 +45,7 @@ export default {
         response = await fetch(destinationURL, init);
 
         const results = await gatherResponse(response);
-        response = new Response(results, init);
+        response = new Response(results);
         response.headers.append("Cache-Control", "s-maxage=60");
         ctx.waitUntil(cache.put(cacheKey, response.clone()));
       }
